41fcffd3df8b63713eadb52e4a11522bc4e94074
karthik S <kartiks0598@gmail.com>
2024-04-06 13:54:43 +0530

    fix(fundMe): add receive & fallback to handle direct 'eth' tansfer

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -29,6 +29,14 @@ contract FundMe {
         i_owner = msg.sender;
     }
 
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
+    }
+
     /// @notice Funds our contract based on the ETH/USD price
     function fund() public payable {
         require(
