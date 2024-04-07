79b1e133fa05161b8df6de8966ceda8b5c728442
karthik S <kartiks0598@gmail.com>
2024-04-07 15:20:23 +0530

    fix(hardhat-config): revised with new deps

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,7 @@
-require("@nomiclabs/hardhat-waffle")
-require("hardhat-gas-reporter")
-require("@nomiclabs/hardhat-etherscan")
+require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
-require("solidity-coverage")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-gas-reporter")
 require("hardhat-deploy")
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -52,16 +51,15 @@ module.exports = {
         // customChains: [], // uncomment this line if you are getting a TypeError: customChains is not iterable
     },
     gasReporter: {
-        enabled: true,
+        enabled: false,
         currency: "USD",
         outputFile: "gas-report.txt",
         noColors: true,
-        // coinmarketcap: COINMARKETCAP_API_KEY,
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
     namedAccounts: {
         deployer: {
             default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
         },
     },
     mocha: {
